ca7dd0eef54e588bab1fe707ae5449773240e6fd
yumaster <yumaster@yeah.net>
2020-10-12 16:55:52 +0800

    init

---
+++ CRCXMODEM/FunList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRCXMODEM
+{
+    /// <summary>
+    /// 命令列表，eg.C03->43 30 33
+    /// 获取枚举描述文本
+    /// </summary>
+    public enum FunList
+    {
+        [DisplayText("C03")]
+        初始化不移动卡,
+
+        [DisplayText("C10")]
+        查卡机状态,
+
+        [DisplayText("C22")]
+        将卡移动到RF卡位,
+
+        [DisplayText("C20")]
+        将卡移到出卡口持卡位,
+
+        [DisplayText("C30")]
+        使能前端进卡,
+
+        [DisplayText("C24")]
+        将卡移动到发卡栈,
+    }
+    /// <summary>
+    /// 枚举描述文本特性
+    /// </summary>
+    public class DisplayText : Attribute
+    {
+        public DisplayText(string Text)
+        {
+            this.text = Text;
+        }
+        private string text;
+        public string Text
+        {
+            get { return text; }
+            set { text = value; }
+        }
+    }
+}
---
+++ CRCXMODEM/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CRCXMODEM
+{
+    class Program
+    {
+        //声明串口类实例
+        private static SerialPort port = null;
+
+        static void Main(string[] args)
+        {
+            // 配置串口
+            port = new SerialPort("COM3");
+            port.BaudRate = 115200;
+            port.DataBits = 8;
+            port.Parity = Parity.Even;
+            port.StopBits = StopBits.One;
+
+            port.Open();
+
+            // 打开
+            if (port.IsOpen)
+            {
+                Console.WriteLine("串口打开成功");
+            }
+            else
+            {
+                Console.WriteLine("串口打开失败");
+            }
+            // 启动接收线程
+            Thread th1 = new Thread(receivedata);
+            if (th1 != null)
+                th1.Start();
+
+            SendData(StrCommon.GetEnumText(FunList.初始化不移动卡));
+            Console.ReadLine();
+
+            SendData(StrCommon.GetEnumText(FunList.查卡机状态));
+            Console.ReadLine();
+
+            SendData(StrCommon.GetEnumText(FunList.将卡移动到RF卡位));
+            Console.ReadLine();
+
+            SendData(StrCommon.GetEnumText(FunList.将卡移到出卡口持卡位));
+            Console.ReadLine();
+
+            SendData(StrCommon.GetEnumText(FunList.使能前端进卡));
+            Console.ReadLine();
+
+            SendData(StrCommon.GetEnumText(FunList.将卡移动到发卡栈));
+            Console.ReadLine();
+        }
+
+        // 接收线程
+        public static void receivedata()
+        {
+            while (true)
+            {
+                byte[] rec = new byte[10];
+                port.Read(rec, 0, 10);//读取缓冲数据
+                string str = StrCommon.GetHexStrByByteArray(rec);
+                Console.WriteLine("接收线程:{0}", str);
+                Thread.Sleep(500);
+            }
+        }
+        // 发送线程
+        public static void SendData(string comStr)
+        {
+            string cmd = StrCommon.GetSerialCommand(comStr);
+            byte[] cmdByteArr = StrCommon.GetByteArrayByHexStr(cmd);
+            port.Write(cmdByteArr, 0, cmdByteArr.Length);
+            Console.WriteLine("发送线程:" + cmd);
+        }
+        
+    }
+}
