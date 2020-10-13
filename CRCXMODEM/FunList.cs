56ba063136bdcc7bc5a311be12fd1853e106a227
yumaster <yumaster@yeah.net>
2020-10-13 18:07:30 +0800

    funlist

--- CRCXMODEM/FunList.cs
+++ CRCXMODEM/FunList.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace CRCXMODEM
 {
+    public class FunLib
+    {
+        public static string 初始化不移动卡 { get { return StrCommon.GetSerialCommand("C03"); } }
+        public static string 查卡机状态 { get { return StrCommon.GetSerialCommand("C10"); } }
+        public static string 将卡移动到RF卡位 { get { return StrCommon.GetSerialCommand("C22"); } }
+        public static string 将卡移到出卡口持卡位 { get { return StrCommon.GetSerialCommand("C20"); } }
+        public static string 使能前端进卡 { get { return StrCommon.GetSerialCommand("C30"); } }
+        public static string 将卡移动到发卡栈 { get { return StrCommon.GetSerialCommand("C24"); } }
+    }
     /// <summary>
     /// 命令列表，eg.C03->43 30 33
     /// 获取枚举描述文本
